4ca2cb75f345d5a5e000085bf12eecdc32c2d736
xskace12 <xskace12@vutbr.cz>
2018-12-10 19:43:13 +0100

    removed commented code from Mesh.cpp

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -54,36 +54,6 @@ const glm::uvec2* Mesh::computeTriangleBoundingBox(glm::uvec2(&pixel_aligned_bou
 
 	return pixel_aligned_boundary_points;
 }
-/*
-void Mesh::ClearMesh()
-{
-	//delete(indices);
-	//delete(vertices);
-	//delete(this);
-}*/
-/*
-bool RT_Mesh::shadowRayHitTriangle(std::vector<glm::vec3> _triangle, Ray *ray, bool _singleSided, float& distance, glm::vec3 & PHit, float min_dist)
-{//TODO edit to use references or pointers?
-	//std::cout << "kreslim vertexy: v0: " << glm::to_string(triangle[0]) << " a v1: " << glm::to_string(triangle[1]) << " a v2: " << glm::to_string(triangle[2]) << " \n";
-	glm::vec3 normal = RT_Mesh::getTriangleNormal(_triangle);//is normalized
-	//std::cout <<"normala: "<< glm::to_string(normal) << " vertex 0 = " << glm::to_string(_triangle[0])<<"\n";
-	float d = getDistanceFromOrigin(normal, _triangle[0]);
-	int e = 0;
-	 d = getPlaneIntersectionDistance(d, normal, ray->origin, ray->direction, _singleSided,e);
-	///Distance > min_dist <-this triangle is further than previously hit
-	if (d <= 0.001f ||d > min_dist||e==1) {
-		return false; // Triangle is behind the camera OR it's parallel with the ray OR it's faced the other way and is single sided - both invisible ///TODO too far - fake dust in air
-	}
-	glm::vec3 _PHit = getPlaneIntersection(ray->origin, d, ray->direction);
-
-	///is Phit inside the triangle? test against each edge
-	if (glm::dot(normal, glm::cross(_triangle[1] - _triangle[0], _PHit - _triangle[0])) < 0) { return false; }
-	if (glm::dot(normal, glm::cross(_triangle[2] - _triangle[1], _PHit - _triangle[1])) < 0) { return false; }
-	if (glm::dot(normal, glm::cross(_triangle[0] - _triangle[2], _PHit - _triangle[2])) < 0) { return false; }
-	PHit = _PHit;
-	distance = d;
-	return true; //successful hit
-}*/
 
 /*
 return > 0 if c is on the left side of v0-v1 line
@@ -168,32 +138,6 @@ bool Mesh::isPixelInTriangle(std::vector<float> tuv, glm::vec3 v0, glm::vec3 v1,
 	}
 	return true;
 }
-/*
-bool Mesh::pixelInTriangle(glm::vec3* _triangle,bool isPrimary, bool _singleSided,float& distance, glm::vec2 &pixel,float min_dist)
-{//TODO edit to use references or pointers?
-
-	glm::vec3 normal = Mesh::calcTriangleNormal(_triangle);//is normalized
-	//float d = getDistanceFromOrigin(normal, _triangle[0]);
-	//int e = 0;
-	//parametr t z P=O+tR
-	//d = (isPrimary ? 1.f : -1.f)* getPlaneIntersectionDistance(isPrimary,d, normal, ray->origin, ray->direction, _singleSided,e);
-	///Distance > min_dist <-this triangle is further than previously hit
-	//d *= (isPrimary? 1.f : -1.f);//camera has negative z, ray doesnt...
-	//std::cout << "vzdalenost: " << d << std::endl;
-	/*if ((d <= (isPrimary? CAM_NEAR_PLANE: 0.f) +0.00f) || (d > min_dist)||(e==1)) {//e==1 odvraceny trojuhelnik,
-		return false; // Triangle is behind the camera OR it's parallel with the ray OR it's faced the other way and is single sided - both invisible
-	}
-	//glm::vec3 _PHit = getPlaneIntersection(ray->origin, d, ray->direction);
-	//_PHit = getPlaneIntersection(ray->origin, d, ray->direction);
-
-	///is Phit inside the triangle? test against each edge
-	if (glm::dot(normal, glm::cross(glm::vec2(_triangle[1]) - glm::vec2(_triangle[0]), pixel - glm::vec2(_triangle[0]))) < 0) { return false; }
-	if (glm::dot(normal, glm::cross(_triangle[2] - _triangle[1], pixel - _triangle[1])) < 0) { return false; }
-	if (glm::dot(normal, glm::cross(_triangle[0] - _triangle[2], pixel - _triangle[2])) < 0) { return false; }
-	//PHit = _PHit;
-	//distance = d;
-	return true; //successful hit
-}*/
 
 Mesh::~Mesh()
 {
